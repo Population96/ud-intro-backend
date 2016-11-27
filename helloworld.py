7df96e0d31e6c85afd67f2881a3eeeca24e1e26d
Population96 <population96@gmail.com>
2016-11-27 14:40:39 -0500

    Completed Quiz part 11

--- helloworld.py
+++ helloworld.py
@@ -9,13 +9,15 @@ form="""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
+        #self.response.headers['Content-Type'] = 'text/html'
         self.response.out.write(form)
 
 class TestHandler(webapp2.RequestHandler):
     def get(self):
         q = self.request.get("q")
         self.response.out.write(q)
+        #self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.out.write(self.request)
 
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/testform', TestHandler)],
